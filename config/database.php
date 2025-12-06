85919f88cc5942430b2e9264d564a2df29286c12
deubaybe.dounia <deubaybe.dounia@ashesi.edu.gh>
2025-12-06 23:36:48 +0000

    done

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'mindlink';
-$username = 'root';
-$password = '';
+$dbname = 'webtech_2025A_deubaybe_dounia';
+$username = 'deubaybe.dounia';
+$password = 'Dou81387';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
